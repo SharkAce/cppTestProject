3b36423d52617035c42a5ee439f347eb236d2b20
SharkAce <charles.marchildon2@gmail.com>
2022-07-22 18:00:07 -0400

    init

---
+++ src/greet.cpp
@@ -0,0 +1,5 @@
+#include "greet.h"
+
+void greet(std::string name){
+  std::cout << "Hello " << name << std::endl;
+}
---
+++ src/greet.h
@@ -0,0 +1,8 @@
+#ifndef GREET_H
+#define GREET_H
+
+#include <string>
+#include <iostream>
+void greet(std::string name);
+
+#endif
